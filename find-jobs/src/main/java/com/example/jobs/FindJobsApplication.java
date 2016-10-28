d548c818d715f00e19b7656dec106c985cc1f8e3
takatanbook <sentonarukaze@yahoo.co.jp>
2016-10-28 10:22:18 +0900

    更新

---
+++ find-jobs/src/main/java/com/example/jobs/FindJobsApplication.java
@@ -0,0 +1,12 @@
+package com.example.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindJobsApplication.class, args);
+	}
+}
